2e42482cb39b9b5a2a05db365c8a1dfc1ff67d53
Beglerov Baurzhan <iowathe3rd@proton.me>
2024-06-04 08:13:35 +0500

    fix(webhook): fixing logic

--- src/webhook/webhook.service.ts
+++ src/webhook/webhook.service.ts
@@ -1,87 +1,69 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import prisma from 'src/lib/db';
-import {MessagesObject, WebhookObject} from 'src/types';
-import {Contact, Direction, MessageType, Status} from "@prisma/client";
-import {WebhookTypesEnum} from "../types/enums";
-import {timestamp} from "rxjs";
+import { MessagesObject, WebhookObject } from 'src/types';
+import { Contact, Direction, MessageType, Status } from '@prisma/client';
+import { WebhookTypesEnum } from '../types/enums';
 
 @Injectable()
 export class WebhookService {
   async handleWebhook(dto: WebhookObject): Promise<void> {
-    for (const entry of dto.entry) {
-      for (const change of entry.changes) {
-        if (change.field === 'messages') {
-          const { contacts, messages } = change.value;
+    //i need a function that will handle the webhook object and save it to the database
+    const { entry } = dto;
+    for (const entryItem of entry) {
+      const { changes } = entryItem;
+      for (const change of changes) {
+        const { field, value } = change;
+        if (field === 'messages') {
+          const {
+            contacts,
+            errors,
+            messages,
+            messaging_product,
+            metadata,
+            statuses,
+          } = value;
 
-          for (const contact of contacts) {
-            try {
-              await prisma.contact.upsert({
-                where: { phoneNumber: contact.wa_id }, // Using phoneNumber as unique identifier
-                update: {
-                  name: contact.profile.name,
-                  updatedAt: new Date(), // Ensure updatedAt is set on update
-                },
-                create: {
-                  name: contact.profile.name,
-                  phoneNumber: contact.wa_id, // Assuming wa_id is a phone number
-                },
-              });
-            } catch (error) {
-              console.error('Error processing contact:', error);
-              // Additional error handling, e.g., logging or notifications
-            }
-          }
-
-          for (const contact of contacts) {
-            try {
-              await prisma.contact.upsert({
-                where: { phoneNumber: contact.wa_id }, // Using phoneNumber as unique identifier
-                update: {
-                  name: contact.profile.name,
-                  updatedAt: new Date(), // Ensure updatedAt is set on update
-                },
-                create: {
-                  name: contact.profile.name,
-                  phoneNumber: contact.wa_id, // Assuming wa_id is a phone number
-                },
-              });
-            } catch (error) {
-              console.error('Error processing contact:', error);
-              // Additional error handling, e.g., logging or notifications
-            }
-          }
-
-          for (const message of messages) {
-            try {
-              await prisma.message.create({
+          const to = metadata[0].phoneNumberId;
+          if (messages.length > 0) {
+            const message = messages[0];
+            const messageContent = this.getMessageContent(message);
+            const senderData = await prisma.contact.findFirst({
+              where: {
+                phoneNumber: message.from,
+              },
+            });
+            if (!senderData) {
+              await prisma.contact.create({
                 data: {
-                  wa_id: message.id,
-                  timeStamp: new Date(Number(message.timestamp) * 1000),// Convert to ISO-8601 format
-                  type: message.type.toUpperCase() as MessageType,
-                  content: message[message.type],
-                  direction: Direction.Inbound,
-                  contact: {
-                    connectOrCreate: {
-                      where: {
-                        phoneNumber: message.from,
-                      },
-                      create: {
-                        name: contacts.find(contact => contact.wa_id === message.from)?.profile.name || '',
-                        phoneNumber: message.from,
-                      }
-                    },
-                  },
-                  context: {
-                    create: {
-                      status: Status.Sent,
-                    }
-                  }
+                  phoneNumber: message.from,
+                  name: contacts[0].profile.name,
                 },
               });
-            } catch (error) {
-              console.error('Error processing message:', error);
-              // Additional error handling, e.g., logging or notifications
             }
+            const contactId = await prisma.contact.findFirst({
+              where: {
+                phoneNumber: message.from,
+              },
+            });
+            await prisma.message.create({
+              data: {
+                content: messageContent,
+                type: message.type.toUpperCase() as MessageType,
+                direction: Direction.Inbound,
+                timeStamp: new Date(),
+                wa_id: message.id,
+                context: {
+                  create: {
+                    status: Status.Sent,
+                  },
+                },
+                contact: {
+                  connect: {
+                    id: contactId.id,
+                  },
+                },
+              },
+            });
           }
         }
       }
@@ -106,7 +88,7 @@ export class WebhookService {
     }
   }
 
-   getMessageContent = (message: MessagesObject) => {
-    return message[message.type]
+  getMessageContent = (message: MessagesObject) => {
+    return message[message.type];
   };
 }
