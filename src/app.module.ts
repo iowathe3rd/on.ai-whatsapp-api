5cd28c2d9ebfeb52317420659235ed3de066189b
Baurzhan <iowathe3rd@proton.me>
2024-06-07 02:24:10 +0500

    feat(modules): boilerplated a message module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WebhookModule } from './modules/webhook/webhook.module';
 import { Logger } from './services/logger.service';
+import { MessagesModule } from './modules/messages/messages.module';
 
 @Module({
-  imports: [WebhookModule],
+  imports: [WebhookModule, MessagesModule],
   controllers: [AppController],
   providers: [AppService, Logger],
 })
